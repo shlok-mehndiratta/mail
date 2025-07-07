0ffa75dfd1f2f1b26c8f9fa1662b4156ab7ebb9f
ASUS-14XOLED\shlok <shlokm05@gmail.com>
2025-07-08 04:32:32 +0530

    Add full functionality to archive/unarchive the mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,6 @@
+let currentEmailId = null;
+let currentMailbox = 'inbox';
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -6,6 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', compose_submit);
+  document.querySelector('#unarchive').addEventListener('click', function(){ 
+    if (currentEmailId !== null) unarchive_mail(currentEmailId);
+  });
+  document.querySelector('#archive').addEventListener('click', function() {
+    if (currentEmailId !== null) archive_mail(currentEmailId);
+  });
   
   // By default, load the inbox
   load_mailbox('inbox');
@@ -17,7 +26,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  document.querySelector('#view-email').style.display = 'none';
+  document.querySelector('#email-details').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -28,11 +37,11 @@ function compose_email() {
 
 // Function to load a specific mailbox
 function load_mailbox(mailbox) {
-  
+  currentMailbox = mailbox; 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#view-email').style.display = 'none';
+  document.querySelector('#email-details').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -46,15 +55,15 @@ function load_mailbox(mailbox) {
       email.forEach(mail => {
           const email_element = document.createElement('div');
           if (mail.read){
-            email_element.className = 'email-item w-100 my-1 btn alert alert-dark border border-dark';
+            email_element.className = 'email-item w-100 my-1 btn alert alert-dark border border-dark justify-content-between align-items-center';
           } else {
-            email_element.className = 'email-item w-100 my-1 btn alert border border-dark';
+            email_element.className = 'email-item w-100 my-1 btn alert border border-dark justify-content-between align-items-center';
           }
           
           email_element.innerHTML = `
-              <div class="float-left"> 
+              <span class="float-left"> 
               <strong>${mail.sender}</strong> - ${mail.subject}
-              </div>
+              </span>
               <span class="text-muted float-right">
                   ${mail.read ? '<span class="badge badge-secondary">Read</span>' : '<span class="badge badge-primary">Unread</span>'}
               ${mail.timestamp}</span>
@@ -69,7 +78,6 @@ function load_mailbox(mailbox) {
           document.querySelector('#emails-view').append(email_element);
       });
   });
-
 }
 
 
@@ -101,30 +109,44 @@ function compose_submit(event) {
 
 
 function view_email(id) {
+  currentEmailId = id;
   document.querySelector('#view-email').innerHTML ='';
 
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#view-email').style.display = 'block';
+  document.querySelector('#email-details').style.display = 'block';
 
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
     console.log(email);
 
-    const emailview = document.createElement('div')
-    emailview.className = 'mail-details'
-    emailview.innerHTML = `
-            <p><strong>From: </strong>${email.sender}</p>
-            <p><strong>To: </strong>${email.recipients}</p>
-            <p><strong>Subject: </strong>${email.subject}</p>
-            <p><strong>Timestamp: </strong>${email.timestamp}</p>
-            <button class="btn btn-sm btn-outline-primary" id="reply">Reply</button>
-            <hr>
-            <p class="mb-3">${email.body}</p>
-            `;
-    document.querySelector('#view-email').append(emailview);
+  if (currentMailbox !== "sent") {
+        if (email.archived) {
+          document.querySelector('#archive').style.display = 'none';
+          document.querySelector('#unarchive').style.display = 'block';
+        } else {
+          document.querySelector('#archive').style.display = 'block';
+          document.querySelector('#unarchive').style.display = 'none';
+        }
+  } else {
+        document.querySelector('#archive').style.display = 'none';
+        document.querySelector('#unarchive').style.display = 'none';
+  }
 
+  const emailview = document.createElement('div')
+  emailview.className = 'mail-details'
+  emailview.innerHTML = `
+          <p><strong>From: </strong>${email.sender}</p>
+          <p><strong>To: </strong>${email.recipients}</p>
+          <p><strong>Subject: </strong>${email.subject}</p>
+          <p><strong>Timestamp: </strong>${email.timestamp}</p>
+          <button class="btn btn-sm btn-outline-primary" id="reply">Reply</button>
+          <hr>
+          <p class="mb-3">${email.body}</p>
+          `;
+  document.querySelector('#view-email').append(emailview);
+    
   });
 
   fetch(`/emails/${id}`, {
@@ -132,4 +154,29 @@ function view_email(id) {
     body: JSON.stringify({
       read: true })
   });
+}
+
+
+function archive_mail(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true })
+  })
+  .then(() => {
+    console.log('Archived successfully');
+    load_mailbox('inbox');
+})
+}
+
+function unarchive_mail(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: false })
+  })
+  .then(() => {
+    console.log('Unarchived successfully');
+    load_mailbox('inbox');
+})
 }
